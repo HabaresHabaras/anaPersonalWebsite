8344fc6974ea895d1dd321662f82bc6980559158
Dali Gala <habaresbit@gmail.com>
2019-08-05 01:14:43 -0400

    Initial website with Materialize, Html, Css, Js file. Mobile responsive

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.slider').slider();
+    $('.carousel').carousel();
+  });
+
+//Need to add a favicon.ico
+//Need to add Jquery initialization
+//Need to load dom with materialize components working before the website is displayed
+//Need to add insta pictures to the carousel
+//Add pictures of ana to website init
+//Fix navbar to the top of the page
+//add a footer
+//add interaction in the site to send message to her
+//add information page for tiocaracoles and productions
+//add page of articles
+//add page of personal projects
+//add opportunity page for future projects to contact her
+//create git repo to have it displayed on github pages on my phone
+
